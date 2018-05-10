afddbe5298302aa9416f7447035c27d1659c2b9f
Gustavo Lizárraga <lizarraga.gux@gmail.com>
2018-05-10 08:37:34 -0400

    Demo vistas simples

---
+++ AND_401_KT_02_Views/app/src/main/java/com/miramicodigo/and_401_kt_views_1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.miramicodigo.and_401_kt_views_1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
