ff603357c84eac3fbede0b6b60a5ccc981af37f8
Gustavo Lizárraga <lizarraga.gux@gmail.com>
2018-05-15 09:37:28 -0400

    Demo CustomViews 2, shapes y selectres

---
+++ AND_401_KT_06_CustomViews2/app/src/main/java/com/miramicodigo/and_401_kt_06_customviews2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.miramicodigo.and_401_kt_06_customviews2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
+    }
+}
