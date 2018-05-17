da4c30e1c2683230d28c3eee454b20cc59e886bc
Gustavo Lizárraga <lizarraga.gux@gmail.com>
2018-05-17 09:31:59 -0400

    Demo funcionalidad con Kotlin

--- AND_401_KT_07_Funcionalidad/app/src/main/java/com/miramicodigo/funcionalidad/MainActivity.kt
+++ AND_401_KT_07_Funcionalidad/app/src/main/java/com/miramicodigo/funcionalidad/MainActivity.kt
@@ -10,15 +10,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //int num1 = etNumber1.getText();
         var num1 = etNumber1.text
         var num2 = etNumber2.text
 
-        btnSuma.setOnClickListener {
+        btnSuma.setOnClickListener{
             if(num1.toString().equals("") || num2.toString().equals("")) {
-                tvResultado.text= "Campos vacios"
+                //tvResultado.setText("Campos vacios")
+                tvResultado.text = "Campos vacios"
             } else {
-                var sum_result= num1.toString().toDouble()+ num2.toString().toDouble()
-                tvResultado.text= sum_result.toString()
+                //double sum_result = Double.parseDouble(num1.getText().toString()) + Double.parseDouble(num2.getText().toString())
+                var sum_result = num1.toString().toDouble() + num2.toString().toDouble()
+                tvResultado.text = sum_result.toString()
             }
         }
 
@@ -26,32 +29,32 @@ class MainActivity : AppCompatActivity() {
             if(num1.toString().equals("") || num2.toString().equals("")) {
                 tvResultado.text = "Campos vacios"
             } else {
-                var minus_result= num1.toString().toDouble() - num2.toString().toDouble()
-                tvResultado.text= minus_result.toString()
+                var resta_result = num1.toString().toDouble() - num2.toString().toDouble()
+                tvResultado.text = resta_result.toString()
             }
         }
+
         btnMultiplica.setOnClickListener {
             if(num1.toString().equals("") || num2.toString().equals("")) {
                 tvResultado.text = "Campos vacios"
-            }else{
-                var multiply_result= num1.toString().toDouble() * num2.toString().toDouble()
-                tvResultado.text= multiply_result.toString()
+            } else {
+                var multi_result = num1.toString().toDouble() * num2.toString().toDouble()
+                tvResultado.text = multi_result.toString()
             }
         }
 
         btnDivide.setOnClickListener {
             if(num1.toString().equals("") || num2.toString().equals("")) {
                 tvResultado.text = "Campos vacios"
-            }else {
+            } else {
                 if(num2.toString().toInt() == 0) {
                     tvResultado.text = "No se puede dividir entre 0"
                 } else {
-                    var division_result= num1.toString().toDouble() / num2.toString().toDouble()
-                    tvResultado.text= division_result.toString()
+                    var divi_result = num1.toString().toDouble() / num2.toString().toDouble()
+                    tvResultado.text = divi_result.toString()
                 }
             }
         }
-
     }
 
 }
