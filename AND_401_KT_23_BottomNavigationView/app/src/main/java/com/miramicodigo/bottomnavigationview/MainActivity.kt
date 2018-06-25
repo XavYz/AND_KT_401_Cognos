2ba48ec4a938e7e633fabb050d98ac5a222196af
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-06-25 14:24:13 -0400

    Demo BottomNavigationView

--- AND_401_KT_23_BottomNavigationView/app/src/main/java/com/miramicodigo/bottomnavigationview/MainActivity.kt
+++ AND_401_KT_23_BottomNavigationView/app/src/main/java/com/miramicodigo/bottomnavigationview/MainActivity.kt
@@ -9,10 +9,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var fm: FragmentManager
+    lateinit var transaction: FragmentTransaction
+    lateinit var fragment: Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        fm = fragmentManager
+        transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.frameContent, MenuUnoFragment()).commit()
+
+        bottomNavigationView.setOnNavigationItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.menu_1 -> fragment = MenuUnoFragment()
+                R.id.menu_2 -> fragment = MenuDosFragment()
+                R.id.menu_3 -> fragment = MenuTresFragment()
+            }
+            transaction = fragmentManager.beginTransaction()
+            transaction.replace(R.id.frameContent, fragment).commit()
+
+            return@setOnNavigationItemSelectedListener true
+        }
 
     }
 }
