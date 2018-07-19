40e5139f7dc5a3cb20e6c465fa0585fddc54b5bc
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2018-07-19 07:30:08 -0400

    Codigo base demo Restful 1

--- AND_401_KT_35_Restful_2/app/src/main/java/com/miramicodigo/restful_2/model/LoginBody.kt
+++ AND_401_KT_35_Restful_2/app/src/main/java/com/miramicodigo/restful_2/model/LoginBody.kt
@@ -2,8 +2,3 @@ package com.miramicodigo.restful_2.model
 
 import com.google.gson.annotations.SerializedName
 
-class LoginBody(
-        @field:SerializedName("id")
-        var userId: String?,
-        var password: String?
-)
--- AND_401_KT_35_Restful_2/app/src/main/java/com/miramicodigo/restful_2/model/Persona.kt
+++ AND_401_KT_35_Restful_2/app/src/main/java/com/miramicodigo/restful_2/model/Persona.kt
@@ -1,10 +1,3 @@
 package com.miramicodigo.restful_2.model
 
 import java.io.Serializable
-
-class Persona(
-        var id: String?,
-        var name: String?,
-        var address: String?,
-        var gender: String?,
-        var token: String?) : Serializable
